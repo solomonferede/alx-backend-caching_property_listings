0f6ca34083b6e54081f0baaf0eb0728bee3412c9
Solomon <solomonf1227@gmail.com>
2026-01-15 14:10:18 +0300

    Remove redundant comment from property_list view while maintaining view-level caching

--- properties/views.py
+++ properties/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .utils import get_all_properties
 
-@cache_page(60 * 15)  # still keep view-level caching
+@cache_page(60 * 15)
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse({"data": properties})
