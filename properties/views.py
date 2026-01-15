2d7b5950e4d073d00af60fd2c68b3d8211a2c981
Solomon <solomonf1227@gmail.com>
2026-01-15 13:57:30 +0300

    Update .gitignore to include cache and log files; modify property_list response format

--- properties/views.py
+++ properties/views.py
@@ -2,8 +2,7 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
 
-
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values()
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})
